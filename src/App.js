482d84265720fb4ca1a2b3a51cd1a550942f1727
Nitish Kumar <79355266+MrNitishroy@users.noreply.github.com>
2023-08-06 13:20:03 +0530

    reneavew

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 
 import './App.css';
 import Header from './components/Header/Header';
+
 import HomePage from './homePage/HomePage';
-import Homepage from "./homePage/HomePage"
 function App() {
   return (
    <>
